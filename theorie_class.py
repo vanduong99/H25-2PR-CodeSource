1a7f51d438143763a53c75b9d51a311051ec4874
rbabari <r.babari@gmail.com>
2024-12-20 15:04:37 -0500

    Initial commit

---
+++ theorie_class.py
@@ -0,0 +1,32 @@
+#Programmation procédural
+# ligne par ligne
+# affectation / boucle / condition /sd ...
+
+# Programation Orienté Objet
+# Encapsulation : regrouper des choses(parametre / methodes) dans des objets
+# Abstraction : créer des types abstraits
+# Heritage : créer des classes enfants à partir des definition de classes parents
+# Polymorphisme .. des objets de types differents se comportent de la meme maniere
+
+# un objet est un regroupement de attributs (data) + methodes (fonctionnalités)
+# un objet est fabriqué à partir d'un modèle (moule) (class)
+
+# Dans un programme principal : créer des objets depuis nos class,
+# on modifie la data des objets, on fait appel aux méthodes
+
+# Dans le cas de librairies (ensemble de classes qu'on importe)
+# : crée des objets / fait appel méthodes
+
+# language de programmation procédural : pascal, fortran, bash, asm, c
+# language orienté objet : c++, java, c#, python, js
+
+# java et c# (version Microsoft de java) :
+# orienté objet (heritage depuis 1 classe, interface)
+# frame work = ensemble de librairie (WEB)
+# multi plateforme (installer la VM et tout code java fonctionnera )
+
+# Python : language script flexible, polyvalent (pleins de librairies c++)
+# proche du language humain
+# syntaxe facile : pas de acolades, points virgules
+# deja compilé (scripts)
+# multitudes de libraires ()
